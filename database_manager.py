4feff9adaf8559ada0b6637b7672c174513666d6
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-21 12:18:50 +0530

    Initialize database connection and create tables if they don't exist

--- database_manager.py
+++ database_manager.py
@@ -10,3 +10,16 @@ DB_CONFIG = {
     'user': 'root',
     'password': 'kalharamax'  
 }
+
+class DatabaseManager:
+    
+    
+    def connect_to_database(self):
+        try:
+            self.connection = mysql.connector.connect(**DB_CONFIG)
+            if self.connection.is_connected():
+                self.setup_database()
+                print("Database connected successfully")
+        except Error as e:
+            print(f"Error connecting to MySQL: {e}")
+            self.connection = None
