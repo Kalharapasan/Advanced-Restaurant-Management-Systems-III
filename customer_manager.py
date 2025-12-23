aea63b071aefdc46de6657991b1e90f8bb190b48
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-23 13:23:20 +0530

    Setup the complete customer management interface

--- customer_manager.py
+++ customer_manager.py
@@ -3,3 +3,9 @@ from tkinter import ttk, messagebox, simpledialog
 from tkcalendar import DateEntry
 import json
 from datetime import datetime, date
+
+class CustomerManager:
+    
+    
+    def setup_customer_interface(self):
+        
