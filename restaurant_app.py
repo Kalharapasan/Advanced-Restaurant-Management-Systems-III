30787c352ea6b80290b316b287639cfb7a2c87cf
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-25 20:26:09 +0530

    Import custom modules

--- restaurant_app.py
+++ restaurant_app.py
@@ -7,3 +7,12 @@ import random
 import os
 import sys
 
+try:
+    from database_manager import DatabaseManager
+    from customer_manager import CustomerManager
+    from menu_manager import MenuManager
+    from analytics_manager import AnalyticsManager, AnalyticsDisplay
+except ImportError as e:
+    print(f"Import error: {e}")
+    print("Make sure all module files are in the same directory as this script")
+    sys.exit(1)
