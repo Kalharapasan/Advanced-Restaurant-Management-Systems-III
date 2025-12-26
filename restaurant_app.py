fc34af2b9ed184243d0fbfadbd914b6d27016473
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-26 10:49:45 +0530

    Receipt section

--- restaurant_app.py
+++ restaurant_app.py
@@ -272,3 +272,4 @@ class RestaurantManagementSystem:
         parent.grid_rowconfigure(1, weight=1)
         parent.grid_columnconfigure(0, weight=1)
         self.setup_quick_stats(parent)
+        self.setup_receipt_section(parent)
