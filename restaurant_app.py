3ae9a9bd913a0818819531a0125b261fc71132a4
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-29 20:52:13 +0530

    Update time display

--- restaurant_app.py
+++ restaurant_app.py
@@ -52,6 +52,7 @@ class RestaurantManagementSystem:
         self.create_menu_bar()
         self.setup_ui()
         self.load_menu_from_database()
+        self.update_time()
     
     def setup_variables(self):
         self.item_vars = {}
