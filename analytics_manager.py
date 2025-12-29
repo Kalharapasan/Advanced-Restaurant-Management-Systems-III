c0ac292a9c736f6fa05fda42b59dd61a98f63a84
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-29 22:06:59 +0530

    Calculate growth percentages

--- analytics_manager.py
+++ analytics_manager.py
@@ -83,3 +83,6 @@ class AnalyticsManager:
     
     def get_detailed_report(self):
         data = self.analytics_data
+        sales_growth = ((data['today_sales'] - data['yesterday_sales']) / data['yesterday_sales']) * 100
+        week_growth = ((data['week_sales'] - data['last_week_sales']) / data['last_week_sales']) * 100
+        month_growth = ((data['month_sales'] - data['last_month_sales']) / data['last_month_sales']) * 100
