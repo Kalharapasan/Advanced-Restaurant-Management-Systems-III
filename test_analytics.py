1b49b108cb1e76339f557e8db2ac557c6cb188dd
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-30 11:48:00 +0530

    Test data generation

--- test_analytics.py
+++ test_analytics.py
@@ -2,3 +2,5 @@ from analytics_manager import AnalyticsManager, AnalyticsDisplay
 def test_analytics_manager():
     print("Testing Analytics Manager...")
     analytics_manager = AnalyticsManager()
+    analytics_manager.refresh_data()
+    
