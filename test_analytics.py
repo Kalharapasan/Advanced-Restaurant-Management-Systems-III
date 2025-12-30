1474f7810fec7670a91f17f2ce12ad271ceb794a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-30 11:47:44 +0530

    Create analytics manager

--- test_analytics.py
+++ test_analytics.py
@@ -1,3 +1,4 @@
 from analytics_manager import AnalyticsManager, AnalyticsDisplay
 def test_analytics_manager():
     print("Testing Analytics Manager...")
+    analytics_manager = AnalyticsManager()
