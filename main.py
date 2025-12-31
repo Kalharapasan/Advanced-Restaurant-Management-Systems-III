1c336487e45b3922dc833141207fb68781c1bb36
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-31 19:21:54 +0530

    Check if all required dependencies are installed

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ from tkinter import messagebox
 import traceback
 
 def check_dependencies():
+    missing_modules = []
+    
+    try:
+        import mysql.connector
+    except ImportError:
+        missing_modules.append("mysql-connector-python")
+    
+    try:
+        import matplotlib
+    except ImportError:
+        missing_modules.append("matplotlib")
+    
+    try:
+        import pandas
+    except ImportError:
+        missing_modules.append("pandas")
+    
+    try:
+        from PIL import Image
+    except ImportError:
+        missing_modules.append("Pillow")
+    
+    return missing_modules
